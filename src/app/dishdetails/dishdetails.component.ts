1e4dcb44aaf0c597c2df181e34b242f5f3c9b36e
Vincenzo Esposito <expovin@gmail.com>
2017-07-23 14:19:08 +0200

    Assignment1

---
+++ src/app/dishdetails/dishdetails.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component'
+
+@Component({
+  selector: 'app-dishdetails',
+  templateUrl: './dishdetails.component.html',
+  styleUrls: ['./dishdetails.component.scss']
+})
+export class DishdetailsComponent implements OnInit {
+
+  constructor() { 
+
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,4 +1,5 @@
 export class Dish {
+
     name: string;
     image: string;
     category: string;
